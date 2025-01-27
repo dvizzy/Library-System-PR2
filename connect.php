7a8a854152ae8204f923bf1ab66786549f57e1d4
Princess Allysa Villareal <siezydtz@gmail.com>
2025-01-27 16:22:46 +0800

    connect.php

--- connect.php
+++ connect.php
@@ -12,21 +12,4 @@ $conn = new mysqli($servername, $username, $password, $dbase);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// SQL query to fetch data
-$sql = "SELECT * FROM tbl_admin";
-$result = $conn->query($sql);
-
-// Check if the query was successful and if there are any records
-if ($result->num_rows > 0) {
-    // Output data for each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
-    }
-} else {
-    echo "0 results found.";
-}
-
-// Close the connection
-$conn->close();
 ?>
